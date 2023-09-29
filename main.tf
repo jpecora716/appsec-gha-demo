dd3b7a62190371017ce8f5527894e8a90c924b6c
Jim Pecora <jpecora@paloaltonetworks.com>
2023-09-29 14:32:02 -0400

    s3 demo test no idea if this will work part 2

--- main.tf
+++ main.tf
@@ -1,6 +1,3 @@
-# Copyright (c) HashiCorp, Inc.
-# SPDX-License-Identifier: MPL-2.0
-
 terraform {
   required_providers {
     aws = {
@@ -15,17 +12,14 @@ provider "aws" {
   region = "us-west-2"
 }
 
-variable "name" {
-}
-
 resource "aws_s3_bucket" "example" {
-  bucket = "company-${var.name}"
+  for_each = file("companies")
+  bucket = "company-${each.value}"
 }
 
 
 resource "aws_s3_bucket_public_access_block" "example" {
   bucket = aws_s3_bucket.example.id
-
   block_public_acls   = false
   block_public_policy = false
 }
